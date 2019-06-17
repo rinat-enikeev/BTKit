75e8dd69792c4eaac2f339f758a91c23fd9ee82f
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-17 15:43:49 +0500

    add voltage

--- BTKit/Source/Devices/Ruuvi/RuuviDevice.swift
+++ BTKit/Source/Devices/Ruuvi/RuuviDevice.swift
@@ -53,6 +53,19 @@ public extension RuuviTag {
         }
     }
     
+    var voltage: Double? {
+        switch self {
+        case .v2(let data):
+            return nil
+        case .v3(let data):
+            return data.voltage
+        case .v4(let data):
+            return nil
+        case .v5(let data):
+            return data.voltage
+        }
+    }
+    
     var uuid: String {
         switch self {
         case .v2(let data):
