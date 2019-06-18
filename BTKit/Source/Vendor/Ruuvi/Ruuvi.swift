3e3998bd3cb800719282c5777ef5e426946b9d33
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-06-18 15:43:03 +0500

    let for scanner

--- BTKit/Source/Vendor/Ruuvi/Ruuvi.swift
+++ BTKit/Source/Vendor/Ruuvi/Ruuvi.swift
@@ -1,7 +1,5 @@
 public struct Ruuvi: BTVendor {
     public static let eddystone = "ruu.vi/#"
     public static let vendorId = 0x0499
-    public static var scanner: BTScanner {
-        return BTScanneriOS(decoders: [RuuviDecoderiOS()])
-    }
+    public static let scanner: BTScanner = BTScanneriOS(decoders: [RuuviDecoderiOS()])
 }
