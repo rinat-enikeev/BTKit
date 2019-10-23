2a163d0230104d6897994656de25f55d648b5c26
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-10-23 08:27:14 +0500

    use foreground instead of scanner

--- BTKit/Source/Devices/BTForeground.swift
+++ BTKit/Source/Devices/BTForeground.swift
@@ -1,4 +1,26 @@
 public struct BTForeground {
-    public let scanner: BTScanner = BTScanneriOS(decoders: [RuuviDecoderiOS()],
-                                                 services: [RuuviNUSService()])
+    let scanner: BTScanner = BTScanneriOS(decoders: [RuuviDecoderiOS()],
+                                          services: [RuuviNUSService()])
+    
+    public var bluetoothState: BTScannerState { return scanner.bluetoothState }
+    
+    @discardableResult
+    public func scan<T: AnyObject>(_ observer: T, options: BTScannerOptionsInfo? = nil, closure: @escaping (T, BTDevice) -> Void) -> ObservationToken {
+        return scanner.scan(observer, options: options, closure: closure)
+    }
+    
+    @discardableResult
+    public func state<T: AnyObject>(_ observer: T, options: BTScannerOptionsInfo? = nil, closure: @escaping (T, BTScannerState) -> Void) -> ObservationToken {
+        return scanner.state(observer, options: options, closure: closure)
+    }
+    
+    @discardableResult
+    public func lost<T: AnyObject>(_ observer: T, options: BTScannerOptionsInfo? = nil, closure: @escaping (T, BTDevice) -> Void) -> ObservationToken {
+        return scanner.lost(observer, options: options, closure: closure)
+    }
+    
+    @discardableResult
+    public func observe<T: AnyObject>(_ observer: T, uuid: String, options: BTScannerOptionsInfo? = nil, closure: @escaping (T, BTDevice) -> Void) -> ObservationToken {
+        return scanner.observe(observer, uuid: uuid, options: options, closure: closure)
+    }
 }
