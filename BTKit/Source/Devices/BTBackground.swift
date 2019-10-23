98af978397581469674bd64b01eef9bc2d0afd84
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-10-23 08:28:22 +0500

    hide scanners

--- BTKit/Source/Devices/BTBackground.swift
+++ BTKit/Source/Devices/BTBackground.swift
@@ -1,9 +1,36 @@
 import Foundation
 
 public struct BTBackground {
-    public let scanner: BTBackgroundScanner = BTBackgroundScanneriOS(services: [RuuviNUSService()], decoders: [RuuviDecoderiOS()])
+    let scanner: BTBackgroundScanner = BTBackgroundScanneriOS(services: [RuuviNUSService()], decoders: [RuuviDecoderiOS()])
     
     public let services: BTServices = BTServices()
+    
+    @discardableResult
+    public func connect<T: AnyObject>(for observer: T, uuid: String, options: BTScannerOptionsInfo? = nil, connected: ((T, BTConnectResult) -> Void)? = nil, heartbeat: ((T, BTDevice) -> Void)? = nil, disconnected: ((T, BTDisconnectResult) -> Void)? = nil) -> ObservationToken? {
+        if scanner.isConnected(uuid: uuid) {
+            let info = BTKitParsedOptionsInfo(options)
+            info.callbackQueue.execute {
+                connected?(observer, .already)
+            }
+            return nil
+        } else {
+            return scanner.connect(observer, uuid: uuid, options: options, connected: { observer, error in
+                if let error = error {
+                    connected?(observer, .failure(error))
+                } else {
+                    connected?(observer, .just)
+                }
+            }, heartbeat: { observer, device in
+                heartbeat?(observer, device)
+            }) { observer, error in
+                if let error = error {
+                    disconnected?(observer, .failure(error))
+                } else {
+                    disconnected?(observer, .just)
+                }
+            }
+        }
+    }
     
     @discardableResult
     public func disconnect<T:AnyObject>(for observer: T, uuid: String, options: BTScannerOptionsInfo? = nil, result: ((T, BTDisconnectResult) -> Void)?) -> ObservationToken? {
@@ -31,33 +58,6 @@ public struct BTBackground {
                     result?(observer, .just)
                 }
             })
-        }
-    }
-    
-    @discardableResult
-    public func connect<T: AnyObject>(for observer: T, uuid: String, options: BTScannerOptionsInfo? = nil, connected: ((T, BTConnectResult) -> Void)? = nil, heartbeat: ((T, BTDevice) -> Void)? = nil, disconnected: ((T, BTDisconnectResult) -> Void)? = nil) -> ObservationToken? {
-        if scanner.isConnected(uuid: uuid) {
-            let info = BTKitParsedOptionsInfo(options)
-            info.callbackQueue.execute {
-                connected?(observer, .already)
-            }
-            return nil
-        } else {
-            return scanner.connect(observer, uuid: uuid, options: options, connected: { observer, error in
-                if let error = error {
-                    connected?(observer, .failure(error))
-                } else {
-                    connected?(observer, .just)
-                }
-            }, heartbeat: { observer, device in
-                heartbeat?(observer, device)
-            }) { observer, error in
-                if let error = error {
-                    disconnected?(observer, .failure(error))
-                } else {
-                    disconnected?(observer, .just)
-                }
-            }
         }
     }
 }
