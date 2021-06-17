fdf135e8199453fbb3ae147c14455aafd615a73a
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-06-18 00:02:54 +0500

    fixes

--- Package.swift
+++ Package.swift
@@ -1,21 +1,23 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "BTKit",
+    defaultLocalization: "en",
     platforms: [
-       .macOS(.v10_13), .iOS(.v10),
+       .macOS(.v10_15), .iOS(.v10),
     ],
     products: [
         .library(
             name: "BTKit",
-            targets: ["BTKit"]),
+            targets: ["BTKit"])
     ],
     targets: [
         .target(
             name: "BTKit",
-            dependencies: [])
+            dependencies: [],
+            resources: [.process("Sources")])
     ]
 )
