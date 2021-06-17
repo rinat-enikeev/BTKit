82bf7427b037bbda13f8a92296fa4be007d84f42
Rinat Enikeev <rinat.enikeev@gmail.com>
2021-06-18 00:45:41 +0500

    move to resources

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
     targets: [
         .target(
             name: "BTKit",
-            dependencies: []),
+            dependencies: [],
+            resources: [.process("Resources")]),
         .testTarget(
             name: "BTKitTests",
             dependencies: ["BTKit"]),
